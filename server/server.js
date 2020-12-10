8b3af63d7c8c265e8472e3f169e79cf3f2a8399e
vinodell <max.khamitov@mail.ru>
2020-12-10 16:54:05 +0300

    server side is done

--- server/server.js
+++ server/server.js
@@ -42,6 +42,25 @@ const toWriteFile = (fileData, category) => {
   writeFile(`${__dirname}/tasks/${category}.json`, JSON.stringify(fileData), { encoding: 'utf8' })
 }
 
+const toReadFile = (category) => {
+  return readFile(`${__dirname}/tasks/${category}.json`, { encoding: 'utf8' }).then((file) =>
+    JSON.parse(file)
+  )
+}
+
+const removeUnderscope = (tasks) => {
+  return tasks
+    .filter((it) => !it._isDeleted)
+    .map((obj) => {
+      return Object.keys(obj).reduce((acc, rec) => {
+        if (rec[0] !== '_') {
+          return { ...acc, [rec]: obj[rec] }
+        }
+        return acc
+      }, {})
+    })
+}
+
 let connections = []
 
 const port = process.env.PORT || 8090
@@ -57,28 +76,6 @@ const middleware = [
 
 middleware.forEach((it) => server.use(it))
 
-// server.post('/api/v1/tasks/:category', async (req, res) => {
-//   const { category } = req.params
-//   const { title } = req.body
-//   const newTask = {
-//     ...taskExample,
-//     taskId: nanoid(),
-//     title,
-//     _createdAt: +new Date()
-//   }
-//   const taskList = await readFile(`${__dirname}/tasks/${category}.json`, { encoding: 'utf8' })
-//     .then((file) => {
-//       const list = [...JSON.parse(file), newTask]
-//       toWriteFile(list, category)
-//       return list
-//     })
-//     .catch(async () => {
-//       await toWriteFile([newTask], category)
-//       return([newTask])
-//     })
-//   res.json(taskList)
-// })
-
 server.post('/api/v1/tasks/:category', async (req, res) => {
   const { category } = req.params
   const { title } = req.body
@@ -88,33 +85,95 @@ server.post('/api/v1/tasks/:category', async (req, res) => {
     title,
     _createdAt: +new Date()
   }
-  const taskList = await readFile(`${__dirname}/tasks/${category}.json`, { encoding: 'utf8' })
-    .then((file) => [...JSON.parse(file), newTask])
+  const taskList = await toReadFile(category)
+    .then((file) => {
+      const list = [...file, newTask]
+      toWriteFile(list, category)
+      return list
+    })
     .catch(async () => {
       await toWriteFile([newTask], category)
-      res.json([newTask])
+      return [newTask]
     })
-  toWriteFile(taskList, category)
   res.json(taskList)
+})
+
+server.get('/api/v1/tasks/:category/:timespan', async (req, res) => {
+  const { category, timespan } = req.params
+  const period = {
+    day: 86400000,
+    week: 604800000,
+    month: 2592000000
+  }
+  const keys = Object.keys(period)
+  const index = keys.indexOf(timespan)
+  if (index < 0) {
+    res.status(404)
+    res.end()
+  }
+  const data = await toReadFile(category)
+    .then((file) => {
+      return file.filter((task) => {
+        return task._createdAt - period[timespan] > +new Date()
+      })
+    })
+    .then((file) => removeUnderscope(file))
+    .catch(() => {
+      res.status(404)
+      res.end()
+    })
+  res.json(data)
 })
 
 server.get('/api/v1/tasks/:category', async (req, res) => {
   const { category } = req.params
-  const filterTask = await readFile(`${__dirname}/tasks/${category}.json`, { encoding: 'utf8' })
-    .then((task) =>
-      JSON.parse(task)
-        .filter((it) => !it._isDeleted)
-        .map((obj) => {
-          return Object.keys(obj).reduce((acc, rec) => {
-            if (rec[0] !== '_') {
-              return { ...acc, [rec]: obj[rec] }
-            }
-            return acc
-          }, {})
-        })
-    )
-    .catch(() => ['no category'])
+  const filterTask = await toReadFile(category)
+    .then((file) => removeUnderscope(file))
+    .catch(() => {
+      res.status(404)
+      res.end()
+    })
   res.json(filterTask)
+})
+
+server.delete('/api/v1/tasks/:category/:id', async (req, res) => {
+  const { category, id } = req.params
+  const newList = await toReadFile(category)
+    .then((file) =>
+      file.map((task) => {
+        return task.taskId !== id ? task : { ...task, _isDeleted: true, _deletedAt: +new Date() }
+      })
+    )
+    .catch(() => {
+      res.status(404)
+      res.end()
+    })
+  toWriteFile(newList, category)
+  res.json(newList)
+})
+
+server.patch('/api/v1/tasks/:category/:id', async (req, res) => {
+  const { category, id } = req.params
+  const { status } = req.body
+  const arrayStatus = ['done', 'new', 'in progress', 'blocked']
+  const check = arrayStatus.includes(status)
+  if (!check) {
+    res.status(501)
+    res.json({ status: 'error', message: 'incorrect status' })
+    res.end()
+  }
+  const data = await toReadFile(category)
+    .then((file) => {
+      return file.map((task) => {
+        return task.taskId === id ? { ...task, status } : task
+      })
+    })
+    .catch(() => {
+      res.status(404)
+      res.end()
+    })
+  toWriteFile(data, category)
+  res.json(data)
 })
 
 server.use('/api/', (req, res) => {
